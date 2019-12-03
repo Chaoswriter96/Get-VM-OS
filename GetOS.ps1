518b6a1af7caae67fee39379dbd69ae6bb44efbe
Chaoswriter96 <steve.egger@bluewin.ch>
2019-12-03 11:03:41 +0100

    Create GetOS.ps1

---
+++ GetOS.ps1
@@ -0,0 +1,15 @@
+﻿$HyperVServer = "Your Hyper-V"
+
+Foreach ($VMGuest in get-vm -ComputerName $HyperVServer)
+		{
+		$NetworkConfig = Get-VMNetworkAdapter -ComputerName $HyperVServer -VMName $VMGuest.Name
+		$serverName = $VMGuest.Name
+		$IP = $NetworkConfig.ipaddresses | where {$_ -notlike "*:*"}
+
+		if (Test-connection $IP )
+			{
+			$query = Get-WmiObject -comp $ip -class Win32_OperatingSystem
+			$OSver = $query.caption
+			Write-Host "$servername IP Address is $ip Running $OSVer"
+			}
+		}
